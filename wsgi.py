723ea51ba8a5a1d84ea28b912c70a08b24d09b0d
Kamal Bin Mustafa <kamal@k4ml.github.io>
2015-04-16 00:38:12 +0800

    use importd

--- wsgi.py
+++ wsgi.py
@@ -15,38 +15,17 @@ except IOError:
 import lupa
 from lupa import LuaRuntime
 
-def application(environ, start_response):
+from importd import d
 
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        lua = LuaRuntime(unpack_returned_tuples=True)
-        body = lua.eval('''[[
-            <html>
-            <body>
-                <h1>Hello world</h1>
-            </body>
-            </html> 
-        ]]''')
-        response_body = str(body)
+@d("/")
+def index(request):
+    lua = LuaRuntime(unpack_returned_tuples=True)
+    body = lua.eval('''[[
+        <h1>hello world</h1>
+    ]]''')
+    return d.HttpResponse(body)
 
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body]
+application = d
 
-#
-# Below for testing only
-#
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, application)
-    # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+if __name__ == "__main__":
+    d.main()
