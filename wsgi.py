febdce38a81331da86fea39f6f293a348300b3f5
Kamal Mustafa <kamal@randomquran.github.io>
2015-07-21 15:53:23 +0800

    fix conflict

--- wsgi.py
+++ wsgi.py
@@ -12,47 +12,6 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
-import lupa
-from lupa import LuaRuntime
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lupi.settings'
 
-from importd import d
-
-CWD = os.path.abspath(os.path.dirname(__file__))
-d(
-    INSTALLED_APPS=[
-        'lupi',
-    ]
-)
-
-class Request(object):
-    def __init__(self, request):
-        self.path = request.path
-        self.method = request.method
-        self.query = request.GET
-        self.form = request.POST
-
-    def get_path(self):
-        return self.path
-
-@d("/")
-def index(request):
-    i_request = Request(request)
-    lua = LuaRuntime(unpack_returned_tuples=True)
-    sandbox = lua.eval("require('sandbox')")
-    lua_script = open(os.path.join(CWD, 'test.lua')).read()
-    result = sandbox.run('function main(request) ' + lua_script + ' end')
-    if result is True:
-        body = sandbox.env["main"](i_request)
-        return d.HttpResponse(body)
-
-    return d.HttpResponse('Failed')
-
-@d("/scripts/new/")
-def new_script(request):
-    if request.method == 'POST':
-        return "new.html"
-
-application = d
-
-if __name__ == "__main__":
-    d.main()
+from lupi.wsgi import application
